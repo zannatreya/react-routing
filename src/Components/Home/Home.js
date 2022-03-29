fba4aa130e9a2d2a9d40d78eed2c0f7c877b659c
zannatreya <zannatulbintabahar@yahoo.com>
2022-03-29 19:21:49 +0600

    initial commit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>this is home</h2>
+            <p>place for ghum</p>
+        </div>
+    );
+};
+
+export default Home;
